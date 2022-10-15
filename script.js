484b6348088fecf683e18fa55f464ba5fd004c07
Moringa School <ruthmutiso69@gmail.com>
2022-10-15 13:47:03 +0300

    Done with html

--- script.js
+++ script.js
@@ -8,3 +8,59 @@ function menutoggle() {
         menulist.style.maxHeight = "0px";
     }
 }
+let menu = document.querySelector('#menulist');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa fa-bars');
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#primary-btn').onclick = () =>{
+  document.querySelector('.BOOK NOW').classList.toggle('fa fa-search');
+}
+
+document.querySelector('#secondary-btn').onclick = () =>{
+  document.querySelector('.CONTACT').classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  };
+
+};
+
+document.querySelector('.home').onmousemove = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    let speed = elm.getAttribute('data-speed');
+
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
+
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+
+  });
+
+};
+
+
+document.querySelector('.home').onmouseleave = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    elm.style.transform = `translateX(0px) translateY(0px)`;
+
+  });
+
+};
+
+
